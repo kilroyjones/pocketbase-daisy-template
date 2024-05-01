603687869ac8a44c33c39e7e5920918d10e03aa2
kilroy <tsbach@gmail.com>
2024-05-01 14:50:12 +0800

    update

--- editor/src/lib/stores/ui.store.ts
+++
@@ -1,5 +0,0 @@
-import { type Writable, writable } from 'svelte/store';
-
-export const showEditor = writable(false);
-export const useSnapGrid: Writable<boolean> = writable(false);
-export const snapGrid: Writable<Array<number>> = writable([25, 25]);
--- viewer/src/lib/stores/ui.store.ts
+++
@@ -1,5 +0,0 @@
-import { type Writable, writable } from 'svelte/store';
-
-export const showContent = writable(true);
-// export const useSnapGrid: Writable<boolean> = writable(false);
-// export const snapGrid: Writable<Array<number>> = writable([25, 25]);
